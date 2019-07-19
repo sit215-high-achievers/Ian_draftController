f572feb131f983a55f3b809a80182bbf212689b3
Riley Baird <riley@mailo.com>
2019-07-19 22:15:44 +1000

    Created performSimulation function to reduce code duplication

--- carSimulation.cpp
+++ carSimulation.cpp
@@ -3,60 +3,37 @@
 #include <iomanip>
 #include "car.h"
 
-#define SIM_ITER_COUNT 100
 #define NUMBER_OF_LINEBREAKS 5
+#define SIM_ITER_COUNT 100
+#define SPEED_LIMIT 50
+
+void performSimulation(Car car, int roadAngle)
+{
+	auto stamp = std::clock();
+	int count = 0;
+	while (count < SIM_ITER_COUNT)
+    {
+        if (std::clock() >= stamp + 100)
+        {
+            std::cout << std::string(NUMBER_OF_LINEBREAKS, '\n');
+            std::cout << "Effective Road Angle: " << roadAngle << "\tSpeed Limit: " << SPEED_LIMIT << std::endl;
+            car.setPower(SPEED_LIMIT);
+            std::cout << "Speed: " << car.getSpeed(roadAngle) << std::endl;
+            std::cout << "Acceleration: " << car.accel << std::endl;
+            count++;
+            stamp = std::clock();
+        }
+    }
+}
 
 int main()
 {
-    auto stamp = std::clock();
-    int count = 0;
-    const int speedLimit = 50;
-    Car car(speedLimit);
+    Car car(SPEED_LIMIT);
 
     std::cout << std::fixed;
     std::cout << std::setprecision(2);
 
-    while (count < SIM_ITER_COUNT)
-    {
-        if (std::clock() >= stamp + 100)
-        {
-            std::cout << std::string(NUMBER_OF_LINEBREAKS, '\n');
-            std::cout << "Effective Road Angle: " << 0 << "\tSpeed Limit: " << speedLimit << std::endl;
-            car.setPower(speedLimit);
-            std::cout << "Speed: " << car.getSpeed(0) << std::endl;
-            std::cout << "Acceleration: " << car.accel << std::endl;
-            count++;
-            stamp = std::clock();
-        }
-    }
-
-    count = 0;
-    while (count < SIM_ITER_COUNT)
-    {
-        if (std::clock() >= stamp + 100)
-        {
-            std::cout << std::string(NUMBER_OF_LINEBREAKS, '\n');
-            std::cout << "Effective Road Angle: " << 5 << "\tSpeed Limit: " << speedLimit << std::endl;
-            car.setPower(speedLimit);
-            std::cout << "Speed: " << car.getSpeed(5) << std::endl;
-            std::cout << "Acceleration: " << car.accel << std::endl;
-            count++;
-            stamp = std::clock();
-        }
-    }
-
-    count = 0;
-    while (count < SIM_ITER_COUNT)
-    {
-        if (std::clock() >= stamp + 100)
-        {
-            std::cout << std::string(NUMBER_OF_LINEBREAKS, '\n');
-            std::cout << "Effective Road Angle: " << 10 << "\tSpeed Limit: " << speedLimit << std::endl;
-            car.setPower(speedLimit);
-            std::cout << "Speed: " << car.getSpeed(10) << std::endl;
-            std::cout << "Acceleration: " << car.accel << std::endl;
-            count++;
-            stamp = std::clock();
-        }
-    }
+	performSimulation(car, 0);
+	performSimulation(car, 5);
+	performSimulation(car, 10);
 }
