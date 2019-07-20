3cbb59349dc0b123286125bcca78e165d0f8f2c4
Ian-RG <30923025+Ian-RG@users.noreply.github.com>
2019-07-20 15:23:58 +1000

    Update fuzzyFunction.cpp

--- fuzzyFunction.cpp
+++ fuzzyFunction.cpp
@@ -1,5 +1,6 @@
 #include "fuzzyFunction.h"
 
+//Triangle function constructor
 FuzzyTriangle::FuzzyTriangle()
 {
     left = 0;
@@ -7,6 +8,7 @@ FuzzyTriangle::FuzzyTriangle()
     middle = 0;
 }
 
+//Sets left, right, and middle x-values for triangle points
 void FuzzyTriangle::setPoints(double l, double r, double m)
 {
     left = l;
@@ -16,18 +18,24 @@ void FuzzyTriangle::setPoints(double l, double r, double m)
 
 double FuzzyTriangle::getValue(double v)
 {
+    //Membership is zero if value is lower than the left side of triangle
     if (v < left)
         return 0;
+    //Calculate membership if point is between left and middle point
     else if (v < middle)
         return (v - left) / (middle - left);
+    //If value is equal to middle point, membership is 1
     else if (v == middle)
         return 1;
+    //Calculate membership if point is between middle and right point
     else if (v < right)
         return (right - v) / (right - middle);
+    //Value is higher than right hand point, membership is zero
     else
         return 0;
 }
 
+//Trapezoid function constructor
 FuzzyTrapezoid::FuzzyTrapezoid()
 {
     left = 0;
@@ -36,6 +44,7 @@ FuzzyTrapezoid::FuzzyTrapezoid()
     rMiddle = 0;
 }
 
+//Sets left, right, middle-left, and middle-right x-values for trapezoid points
 void FuzzyTrapezoid::setPoints(double l, double r, double lMid, double rMid)
 {
     left = l;
@@ -46,14 +55,19 @@ void FuzzyTrapezoid::setPoints(double l, double r, double lMid, double rMid)
 
 double FuzzyTrapezoid::getValue(double v)
 {
+    //Membership is zero if value is lower than the left side of trapezoid
     if (v < left)
         return 0;
+    //Calculate membership if point is between left and middle-left point
     else if (v < lMiddle)
         return (v - left) / (lMiddle - left);
+    //If the value is between the two middle points, membership is 1
     else if (v >= lMiddle && v <= rMiddle)
         return 1;
+    //Calculate membership if point is between middle-right and right point
     else if (v < right)
         return (right - v) / (right - rMiddle);
+    //Value is higher than right hand point, membership is zero
     else
         return 0;
 }
